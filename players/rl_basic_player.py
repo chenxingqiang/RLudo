6be89bf2bf0b58cbffa4ea88e555b1659acc0a77
Pajaraja <34302662+Pajaraja@users.noreply.github.com>
2020-08-06 17:32:43 +0000

    Add files via upload

---
+++ players/rl_basic_player.py
@@ -0,0 +1,11 @@
+import random
+
+
+class RLBasicPlayer(object):
+
+    def __init__(self):
+        
+
+
+    def play(self, state, num_actions):
+        return self.random.randrange(0, num_actions)
---
+++ training.py
@@ -0,0 +1,5 @@
+from game_loop import loop
+
+
+if __name__ == '__main__':
+    loop()
