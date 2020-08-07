ba680ab2e3a43ebcfccb60cd6d42de238b7de330
Pajaraja <pajam2000@gmail.com>
2020-08-07 10:12:01 +0000

    mini izmene

--- src/environment.py
+++ src/environment.py
@@ -28,12 +28,15 @@ class Ludo(object):
         :param num_players: Number of players
         :return: Initial state
         """
+        
+        device = torch.device('cuda:0' if torch.cuda.is_available() else 'cpu')
+        torch.cuda.set_device(device)
+
         self.ply = 0
-        self.device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
         self.num_players = num_players
-        self.positions = torch.ones(size=(MAX_PLAYERS, TOKENS_PER_PLAYER), dtype=torch.long, device=self.device) * (-1)
-        self.board_state = torch.zeros(BOARD_LENGTH, dtype=torch.long, device=self.device)
-        self.starts = torch.zeros(MAX_PLAYERS, dtype=torch.long, device=self.device)
+        self.positions = torch.ones(size=(MAX_PLAYERS, TOKENS_PER_PLAYER), dtype=torch.long) * (-1)
+        self.board_state = torch.zeros(BOARD_LENGTH, dtype=torch.long)
+        self.starts = torch.zeros(MAX_PLAYERS, dtype=torch.long)
         if self.num_players == 2:
             self.starts[0] = 0
             self.starts[1] = 2 * START_DISTANCE
@@ -46,8 +49,8 @@ class Ludo(object):
             self.starts[1] = 2 * START_DISTANCE
             self.starts[2] = 1 * START_DISTANCE
             self.starts[3] = 3 * START_DISTANCE
-        self.passed = torch.zeros(size=(MAX_PLAYERS, TOKENS_PER_PLAYER), dtype=torch.long, device=self.device)
-        self.home_state = torch.zeros(size=(MAX_PLAYERS, TOKENS_PER_PLAYER), dtype=torch.long, device=self.device)
+        self.passed = torch.zeros(size=(MAX_PLAYERS, TOKENS_PER_PLAYER), dtype=torch.long)
+        self.home_state = torch.zeros(size=(MAX_PLAYERS, TOKENS_PER_PLAYER), dtype=torch.long)
         self.current_player = 0
         self.roll_dice()
 
@@ -131,7 +134,7 @@ class Ludo(object):
         Returns full current board state as a tensor
         :return: Tensor to be passes as input to nn
         """
-        dice_hot = torch.zeros(size=[DICE_MAX], dtype=torch.long, device=self.device)
+        dice_hot = torch.zeros(size=[DICE_MAX], dtype=torch.long)
         dice_hot[self.roll] = 1
         return torch.cat((self.board_state,
                           torch.flatten(self.home_state),
--- src/training.py
+++ src/training.py
@@ -4,37 +4,66 @@ from players.random_player import RandomPlayer
 import random
 
 TOKENS = 4
-EPISODES = 20
+EPISODES = 100
+PLAYERS = 2
 
-
-def iterate(state, agent1, agent2):
+def iterate(state, agent1, agent2,agent3=None,agent4=None):
     game_end = False
-    env.reset(2)
+    env.reset(PLAYERS)
     while not game_end:
         x = env.current_player
         if x == 0:
             action = agent1.play(state, TOKENS)
         if x == 1:
             action = agent2.play(state, TOKENS)
+        if x == 2:
+            action = agent3.play(state, TOKENS)
+        if x == 3:
+            action = agent4.play(state, TOKENS)
         nextstate, reward, game_end = env.step(action)
         if x == 0:
             agent1.recalculate(nextstate, reward)
         if x == 1:
             agent2.recalculate(nextstate, reward)
+        if x == 2:
+            agent3.recalculate(nextstate, reward)
+        if x == 3:
+            agent4.recalculate(nextstate, reward)
 
 
 if __name__ == '__main__':
-    env = Ludo(2)
-    agent1 = RLBasicPlayer(env)
-    agent2 = RLBasicPlayer(env)
-    state = env.current_state()
-    for i in range(EPISODES):
-        iterate(state, agent1, agent2)
-        print('Episode ' + str(i) + ': Player ', env.winning_player + 1, ' wins')
-    win = 0
-    agentr = RandomPlayer()
-    for i in range(EPISODES):
-        iterate(state, agent1, agentr)
-        if env.winning_player == 1:
-            win += 1
-    print('winrate ', win / EPISODES)
+    env = Ludo(PLAYERS)
+    if PLAYERS==2:
+        agent1 = RLBasicPlayer(env)
+        agent2 = RLBasicPlayer(env)
+        state = env.current_state()
+        for i in range(EPISODES):
+            iterate(state, agent1, agent2)
+            print('Episode ' + str(i) + ': Player ', env.winning_player + 1, ' wins')
+        win = 1
+        agent1.epsilon=0
+        agentr = RandomPlayer()
+        for i in range(EPISODES):
+            iterate(state, agent1, agentr)
+            if env.winning_player == 1:
+                win += 1
+        print('winrate ', win / EPISODES)
+    if PLAYERS==4:
+        agent1 = RLBasicPlayer(env)
+        agent2 = RLBasicPlayer(env)
+        agent3 = RLBasicPlayer(env)
+        agent4 = RLBasicPlayer(env)
+        state = env.current_state()
+        for i in range(EPISODES):
+            iterate(state, agent1, agent2, agent3, agent4)
+            print('Episode ' + str(i) + ': Player ', env.winning_player + 1, ' wins')
+        win = 0
+        agent1.epsilon=0
+        agentr1 = RandomPlayer()
+        agentr2 = RandomPlayer()
+        agentr3 = RandomPlayer()
+        for i in range(EPISODES):
+            iterate(state, agent1, agentr1, agentr2, agentr3)
+            if env.winning_player == 1:
+                win += 1
+        print('winrate ', win / EPISODES)
